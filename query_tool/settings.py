a94a7d5ecc78c4ed495f4614a36b9d12b5889061
NasrShamalkh <naaaa9sr-@hotmail.com>
2020-10-31 01:16:02 +0200

    '...'

--- query_tool/settings.py
+++ query_tool/settings.py
@@ -1,7 +1,7 @@
-import django_heroku
 
 from pathlib import Path
 import os.path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,7 +142,6 @@ USE_TZ = True
 
 # configure app for heroku deploument
 django_heroku.settings(locals())
-
 
 
 STATIC_URL = '/static/'
